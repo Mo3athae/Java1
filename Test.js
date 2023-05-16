270170e3dde493acad69d397053e7f36a7534c20
Muath <Muath.alhashmi@edgegroup.ae>
2023-05-16 15:13:30 +0400

    ex2

--- Test.js
+++ Test.js
@@ -1,11 +1,31 @@
 console.log("This is JS")
-var age = 24;
-var myName = "Muath \n";
-console.log(myName,age)
-console.log(myName.toUpperCase())
-console.log(myName.charAt(0));
-console.log(myName.indexOf("T"));
+// var age = 24;
+// var myName = "Muath \n";
+// console.log(myName,age)
+// console.log(myName.toUpperCase())
+// console.log(myName.charAt(0));
+// console.log(myName.indexOf("T"));
 
-var longSt = "This is a ver long";
-var Checkincl = longSt.includes("i");
-console.log("Is it?", Checkincl);
+// var longSt = "This is a ver long";
+// var Checkincl = longSt.includes("i");
+// console.log("Is it?", Checkincl);
+
+var ToBeEval = "false";
+
+var Value = eval(ToBeEval);
+
+if (typeof Value === "string") 
+{
+    var lastChar = Value.charAt(Value.length-1);
+console.log(lastChar);
+}
+else if(isNaN(Value))
+    console.log("I cannot proceed!");
+else if(!isNaN(Value))
+{
+Value *=3;
+console.log(Value);
+}
+
+else 
+console.log(Value);
