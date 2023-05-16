5e06993cc865d0d6020b616b3c5d3c50026ae173
Muath <Muath.alhashmi@edgegroup.ae>
2023-05-16 12:22:29 +0400

    changes

--- Test.js
+++ Test.js
@@ -1,1 +1,11 @@
 console.log("This is JS")
+var age = 24;
+var myName = "Muath \n";
+console.log(myName,age)
+console.log(myName.toUpperCase())
+console.log(myName.charAt(0));
+console.log(myName.indexOf("T"));
+
+var longSt = "This is a ver long";
+var Checkincl = longSt.includes("i");
+console.log("Is it?", Checkincl);
